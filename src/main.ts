97e95ae9f6d3e279ca9d0e6f63683c311cdfc19b
devjohn42 <devjohncb@gmail.com>
2025-06-16 22:42:28 -0300

    :wrench: chore: configures to access environment variables in any application file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // desabilitar quando houver muitos logs
     // logger: false,
   })
-  await app.listen(process.env.PORT ?? 3333)
+
+  const configService: ConfigService<Env, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
+
 bootstrap()
