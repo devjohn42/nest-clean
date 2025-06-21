1cae9e71a9a806f99795118965159bdf985147f4
devjohn42 <devjohncb@gmail.com>
2025-06-20 22:17:04 -0300

    :sparkles: feat: creates an isolated database for running tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/*.e2e-spec.ts'],
+    include: ['**/*.e2e.test.ts'],
     globals: true,
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
     tsconfigPaths(),
