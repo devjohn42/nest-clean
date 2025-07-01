81ac3f767db6017cb0baf3df958181b786ad0aa4
devjohn42 <devjohncb@gmail.com>
2025-07-01 20:39:07 -0300

    :recycle: refactor: refactor the authentication controller

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,4 +1,5 @@
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
@@ -7,6 +8,7 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students.repository'
 
 @Module({
   providers: [
@@ -14,6 +16,10 @@ import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions
     {
       provide: QuestionsRepository,
       useClass: PrismaQuestionRepository,
+    },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository
     },
     PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
@@ -29,6 +35,7 @@ import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions
     QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
+    StudentsRepository
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,9 @@
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { ListRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/list-recent-questions'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { Module } from '@nestjs/common'
+import { CryptographyModule } from '../criptography/cryptgraphy.module'
 import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
@@ -8,13 +11,18 @@ import { CreateQuestionController } from './controllers/create-question.controll
 import { ListRecentQuestionsController } from './controllers/list-recent-questions.controller'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     ListRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase, ListRecentQuestionsUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    ListRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase
+  ],
 })
-export class HttpModule {}
+export class HttpModule { }
