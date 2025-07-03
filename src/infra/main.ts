b890900be63ceee6fb28d074d810bd1f445e3cd5
devjohn42 <devjohncb@gmail.com>
2025-07-03 01:40:53 -0300

    :sparkles: feat: creates envModule

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,7 +1,6 @@
-import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { Env } from './env'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,8 +8,8 @@ async function bootstrap() {
     // logger: false,
   })
 
-  const configService: ConfigService<Env, true> = app.get(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
 
   await app.listen(port)
 }
