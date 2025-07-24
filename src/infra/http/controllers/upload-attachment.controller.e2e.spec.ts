3fd55b2591e97d24fc7e02f5a35fba8b7b13c635
devjohn42 <devjohncb@gmail.com>
2025-07-23 21:42:41 -0300

    :white_check_mark: test: e2e attachment test upload with cloudflare

--- src/infra/http/controllers/upload-attachment.controller.e2e.spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e.spec.ts
@@ -34,5 +34,8 @@ describe('Upload attachment (E2E)', () => {
       .attach('file', './test/e2e/img-upload.jpg')
 
     expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String)
+    })
   })
 })
